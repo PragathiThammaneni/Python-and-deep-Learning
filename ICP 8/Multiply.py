468d8696302f48c1c09d51cd624817991a1509f2
PragathiThammaneni <31286022+PragathiThammaneni@users.noreply.github.com>
2018-07-03 12:44:49 -0500

    Update Multiply.py

--- ICP 8/Multiply.py
+++ ICP 8/Multiply.py
@@ -1,13 +1,27 @@
-import tensorflow as tf # imported the tensor flow package
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-session = tf.Session() # created a session and 3 matrices
+
+# import TensorFlow library
+import tensorflow as tf
+
+# constant matrix inputs a, b, c
 A = tf.constant([1,5,3,2],shape=[2,2])
 B = tf.constant([1,7,4,2],shape=[2,2])
 C = tf.constant([3,4,5,9],shape=[2,2])
 # function (a^2+b)*c and a, b, and c should be the matrices
-D = tf.pow(A,2)
-E = tf.add(D,B)
-F = tf.multiply(E,C)
-# Printing the output generated
-print(session.run(F))
+# calculating (a^2+b)*c
+d = tf.pow(a, 2, name='d')
+e = tf.add(d,b,name='e')
+f = tf.matmul(e,c,name='output')
+
+# printing the input matrices
+with tf.Session() as session:
+    print("Matrix a: ")
+    print(session.run(a))
+    print("Matrix b: ")
+    print(session.run(b))
+    print("Matrix c: ")
+    print(session.run(c))
+
+# printing the output
+with tf.Session() as session:
+    print("Output Matrix: ")
+    print(session.run(f))
